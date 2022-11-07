fcacbec5932329a4213f24a8f838974ca79ac82a
Geovaunie Golding <geovauniegolding2@gmail.com>
2022-11-06 22:44:28 -0600

    Fixed the Products Route and Controller File.

--- models/products.js
+++ models/products.js
@@ -1,24 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
 
-const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        image: {
+            data: Buffer,
+            contentType: String
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: String,
+            required: true
+        },
+        rating: {
+            type: Array,
+            required: false
+        },
+        main_categoryId: {
+            type: Schema.Types.ObjectId, ref:"MainCategory",
+            required: true,
+        },
+        sub_categoryId: {
+            type: Schema.Types.ObjectId, ref:"SubCategory",
+            required: true,
+        }
     },
-    image: {
-        data: Buffer,
-        contentType: String
-    },
-    price: {
-        type: String,
-        required: true
-    },
-    category: {
-        type: String,
-        required: true
 
-    }
-});
+    { timestamps: true, collection: 'Products' }
+);
 
 
 module.exports = mongoose.model('Products', productSchema);
