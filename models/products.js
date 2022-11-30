f3d03dec35ad287dfc26c98c7d9bc0b628753c79
Roshane <39389600+Roshane-Johnson@users.noreply.github.com>
2022-11-30 10:58:29 -0500

    fixed products controller

--- models/products.js
+++ models/products.js
@@ -2,39 +2,39 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const productSchema = new mongoose.Schema(
-    {
-        name: {
-            type: String,
-            required: true
-        },
-        image: {
-            data: Buffer,
-            contentType: String
-        },
-        description: {
-            type: String,
-            required: true
-        },
-        price: {
-            type: String,
-            required: true
-        },
-        rating: {
-            type: Number,
-            required: false
-        },
-        main_categoryId: {
-            type: Schema.Types.ObjectId, ref:"MainCategory",
-            required: false,
-        },
-        sub_categoryId: {
-            type: Schema.Types.ObjectId, ref:"SubCategory",
-            required: false,
-        }
-    },
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		image: {
+			type: String,
+		},
+		description: {
+			type: String,
+			required: true,
+		},
+		price: {
+			type: String,
+			required: true,
+		},
+		rating: {
+			type: Number,
+			required: false,
+		},
+		main_categoryId: {
+			type: Schema.Types.ObjectId,
+			ref: 'MainCategory',
+			required: false,
+		},
+		sub_categoryId: {
+			type: Schema.Types.ObjectId,
+			ref: 'SubCategory',
+			required: false,
+		},
+	},
 
-    { timestamps: true, collection: 'Products' }
-);
+	{ timestamps: true, collection: 'Products' }
+)
 
-
-module.exports = mongoose.model('Products', productSchema);
+module.exports = mongoose.model('Products', productSchema)
